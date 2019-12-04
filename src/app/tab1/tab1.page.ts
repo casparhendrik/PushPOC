b8799d267aaa92c209254b8dbefade9bb8bb8122
casparhendrik <casparhendrik@hotmail.nl>
2019-12-04 10:53:23 +0100

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Push, PushOptions, PushObject } from '@ionic-native/push/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,42 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+constructor(private push: Push, private plt: Platform) {
+    this.push.hasPermission().then((res: any) => {
+      if (res.isEnabled) {
+        console.log('We have permission to send push notifications');
+      } else {
+        console.log('We do not have permission to send push notifications.');
+      }
+    });
+    this.plt.ready().then(() => {
+      this.pushSetup();
+    });
+  }
+
+   pushSetup() {
+   const options: PushOptions = {
+      android: {
+        senderID: '343566679165'
+      },
+      ios: {
+          alert: 'true',
+          badge: true,
+          sound: 'false'
+      },
+      windows: {},
+      browser: {
+          pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+      }
+    };
+
+   const pushObject: PushObject = this.push.init(options);
+   pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
+   pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+   pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+  }
+
+
+createPush() {}
 
 }
